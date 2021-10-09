8fb79260e171fc42d74873495483726e0ffc4443
Abdirahman <rizetracker@gmail.com>
2021-10-09 21:14:15 +0200

    getAnimeDetails + getAnimeBySearchValue

--- API/anime.py
+++ API/anime.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 import requests
 
 BASEURL = "https://gogoanime.pe/"
@@ -48,3 +48,42 @@ def getPopularAnime(page = 1):
         anime["img"] = element.div.img.attrs['src']
         popularAnime.append(anime)
     return popularAnime
+
+# get the corresponding anime back based on the search value
+# value defaults to ""
+def getAnimeBySearchValue(value = ""):
+    params = { "keyword" : value}
+    html = requests.get(url = f'{BASEURL}/search.html', params = params).text
+    soup = BeautifulSoup(html, 'html.parser')
+    searchResults = []
+    for element in soup.select("ul.items li"):
+        anime = {}
+        anime["id"] = element.p.a.attrs['href'][10:]
+        anime["name"] = element.div.a.attrs['title']
+        anime["img"] = element.div.img.attrs['src']
+        searchResults.append(anime)
+    return searchResults
+
+# get the details of an anime based on the id 
+# id defaults to ""
+def getAnimeDetails(id = ""):
+    html = requests.get(url = f'{BASEURL}/category/{id}').text
+    soup = BeautifulSoup(html, 'html.parser')
+    img = soup.select("div.anime_info_body_bg img")[0].attrs['src']
+    name = soup.select("div.anime_info_body_bg h1")[0].string
+    extra = soup.select("p.type")
+    type = extra[0].a.attrs['title']
+    plot_summary = extra[1].contents[1]
+    released = extra[3].contents[1]
+    status = extra[4].a.string
+    genres = [element.string for element in extra[2].find_all("a")]
+    anime = {
+        "img" : img,
+        "name": name,
+        "plot summary" : plot_summary,
+        "type" : type,
+        "status": status,
+        "released": released,
+        "genres": genres
+    }
+    return anime
--- main.py
+++ main.py
@@ -17,10 +17,22 @@ def animeByGenre(genre):
     return json.dumps(results)
 
 @app.route("/popular")
-def popularAnime(page = 1):
+def popularAnime():
     page = int(request.args.get("page")) if request.args.get("page") is not None else 1
     results = getPopularAnime(page)
     return json.dumps(results)
+
+@app.route("/search")
+def searchAnime():
+    searchValue = request.args.get("keyword")
+    results = getAnimeBySearchValue(searchValue)
+    return json.dumps(results)
+
+@app.route("/details/<id>")
+def animeDetails(id):
+    result = getAnimeDetails(id)
+    return json.dumps(result)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port= 8080)
